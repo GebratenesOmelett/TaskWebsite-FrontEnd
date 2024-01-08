173d970d363477eca7a3ebb720ad6a8579a9c3c2
Aleksy Biela <aleks.302@wp.pl>
2024-01-08 23:21:54 +0100

    Project - Start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {MainWebsiteComponent} from "./components/main-website/main-website.component";
+import {LoginFormComponent} from "./components/login-form/login-form.component";
+import {RegistrationFormComponent} from "./components/registration-form/registration-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegistrationFormComponent},
+  {path: 'login', component: LoginFormComponent},
+  {path: 'home', component: MainWebsiteComponent},
+  {path: '', redirectTo: '/home', pathMatch:"full"},
+  {path: '**', redirectTo: '/home', pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { MainWebsiteComponent } from './components/main-website/main-website.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    LoginFormComponent,
+    RegistrationFormComponent,
+    NavbarComponent,
+    TasksComponent,
+    MainWebsiteComponent
   ],
   imports: [
     BrowserModule,
